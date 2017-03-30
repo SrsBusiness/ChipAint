2724d6d7a1bd5fb27754f3ca73110cc94a90f6f0
Mukund Kidambi <mukkid@gmail.com>
2017-03-30 16:12:41 -0500

    added basic init and opcode fetching

--- chip.c
+++ chip.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include "chip.h"
 
+int init(){
+	I = 0x0;
+	stackPointer = 0;
+	delayTimer = 0;
+	pc = 0x200;
+	return 1;
+}
+
+int run(){
+
+	//fetch opcode
+	opcode = memory[pc] << 8 | memory[pc + 1];
+	printf("%x: ", opcode);
+	
+	//decode opcode
+	switch(opcode & 0xF000){
+		default:
+			printf("Unsupported Opcode\n");
+			return 0;
+	}
+}
 
 int main(){
-	printf("Hello World");
+	init();
+	run();
 	return 1;
 }
