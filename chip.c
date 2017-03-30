6a3671dc5386d04482c4304ad14472a9ede4a415
Mukund Kidambi <mukkid@gmail.com>
2017-03-30 18:54:55 -0500

    added terminal art video output

--- chip.c
+++ chip.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
 #include "chip.h"
 
+
 int init(){
 	I = 0x0;
 	stackPointer = 0;
 	delayTimer = 0;
 	pc = 0x200;
 	return 1;
+}
+
+int load(char *rom){
+	FILE *file = fopen(rom, "r");
+
+	return 1;
+}
+
+int draw(){
+	for (int j = 0; j < 100; j++){
+		printf("\n");
+	}
+	int i = 0;
+	while(i < 2048){
+		if (i%64 == 0){
+			printf("\n");
+		}
+		if (display[i] == 1){
+			printf("#");
+		}
+		else{
+			printf(" ");
+		}
+		i++;
+
+	}
 }
 
 int run(){
@@ -23,8 +50,10 @@ int run(){
 	}
 }
 
-int main(){
+int main(int argc, char **argv){
 	init();
+	load(argv[1]);
+	draw();
 	run();
 	return 1;
 }
